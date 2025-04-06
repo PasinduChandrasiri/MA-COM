976cf5cf41733f9ea68c77b713a40001cbc69a01
Pramuda Sandeepa Kulathunga <pramudakulathunga@gmail.com>
2025-04-07 00:45:18 +0530

    Delete frontend directory

--- frontend/src/Pages/index.js
+++
@@ -1,7 +0,0 @@
-import HomePage from "./HomePage/HomePage";
-import ContactUs from "./ContactUs/ContactUs";
-import Login from './Login/Login';
-import ForgotPassword from './ForgotPassword/ForgotPassword';
-import SignUp from "./SignUp/SignUp";
-
-export { HomePage, ContactUs, Login, ForgotPassword, SignUp }
